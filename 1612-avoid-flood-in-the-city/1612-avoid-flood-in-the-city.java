1f1de217e18fe466cab4c2f64194a44296f42c30
Shivangi Srivastava <srivastavashivangi7757@gmail.com>
2025-10-08 21:05:47 +0530

    Time: 30 ms (98.17%), Space: 63.7 MB (14.16%) - LeetHub

--- 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.java
+++ 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.java
@@ -1,26 +1,54 @@
 class Solution {
+    public int[] avoidFlood(int[] rain) {
+        int n = rain.length;
+        UnionFind uf = new UnionFind(n + 1);
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[n];
+        Arrays.fill(res, 1);
 
-    public int[] avoidFlood(int[] rains) {
-        int[] ans = new int[rains.length];
-        Arrays.fill(ans, 1);
-        TreeSet<Integer> st = new TreeSet<Integer>();
-        Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
-        for (int i = 0; i < rains.length; ++i) {
-            if (rains[i] == 0) {
-                st.add(i);
+        for (int i = 0; i < n; i++) {
+            int lake = rain[i];
+            if (lake == 0) continue;
+
+            res[i] = -1;
+            uf.unite(i);
+
+            if (map.containsKey(lake)) {
+                int prev = map.get(lake);
+                int dry = uf.find(prev + 1);
+
+                if (dry >= i) return new int[0];
+
+                res[dry] = lake;
+                uf.unite(dry);
+                map.put(lake, i);
             } else {
-                ans[i] = -1;
-                if (mp.containsKey(rains[i])) {
-                    Integer it = st.ceiling(mp.get(rains[i]));
-                    if (it == null) {
-                        return new int[0];
-                    }
-                    ans[it] = rains[i];
-                    st.remove(it);
-                }
-                mp.put(rains[i], i);
+                map.put(lake, i);
             }
         }
-        return ans;
+
+        return res;
+    }
+}
+
+class UnionFind {
+    int[] parent;
+
+    public UnionFind(int size) {
+        parent = new int[size + 1];
+        for (int i = 0; i <= size; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int find(int i) {
+        if (parent[i] == i)
+            return i;
+        parent[i] = find(parent[i]);
+        return parent[i];
+    }
+
+    public void unite(int i) {
+        parent[i] = find(i + 1);
     }
 }
